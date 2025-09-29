e77cf705a05443cda9d613baabbd87fafc79a9fe
Matt Shanahan <mrshanahan11235@gmail.com>
2025-09-29 02:10:34 -0500

    Add Git SHA as label on images

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,14 +10,16 @@ validate() {
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+GIT_SHA=$(git rev-parse HEAD)
+
 echo "[build.sh] building auth service images" >&2
-docker compose -f "$SCRIPT_DIR/../docker-compose-auth.yml" build --no-cache
+GIT_SHA=$GIT_SHA docker compose -f "$SCRIPT_DIR/../docker-compose-auth.yml" build --no-cache
 validate "failed to build auth service images"
 
 echo "[build.sh] building api service images" >&2
-docker compose -f "$SCRIPT_DIR/../docker-compose-api.yml" build --no-cache
+GIT_SHA=$GIT_SHA docker compose -f "$SCRIPT_DIR/../docker-compose-api.yml" build --no-cache
 validate "failed to build api service images"
 
 echo "[build.sh] building auth CLI image" >&2
-docker build auth-cli -t notes-api/auth-cli --no-cache
+docker build auth-cli -t notes-api/auth-cli --build-arg GIT_SHA=$GIT_SHA --no-cache
 validate "failed to build auth-cli image"
